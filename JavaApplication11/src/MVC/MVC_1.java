a0dc745dd54a8f7ee2a53d827144ff774f3bea2f
cristian97406 <59977082+cristian97406@users.noreply.github.com>
2020-01-20 09:27:05 -0500

    MVC

---
+++ JavaApplication11/src/MVC/MVC_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+
+import Modelo.persona;
+
+
+/**
+ *
+ * @author crist
+ */
+public class MVC_1 {
+    
+    public static void main(String[] args) {
+        
+        persona cristian = new persona ("cristian","cordova",0,0,0, "la 41", 'm', true);
+        
+        System.out.println(cristian);
+    }
+}
