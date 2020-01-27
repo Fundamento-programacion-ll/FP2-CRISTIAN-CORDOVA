8adf3448b7254ae2ff5c6c340d5712094fbd3820
cristian97406 <59977082+cristian97406@users.noreply.github.com>
2020-01-27 09:53:22 -0500

    MVC

---
+++ conexionBDD/src/conexion/conector.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author crist
+ */
+public class conector {
+    
+    Connection conexion;
+    
+    String url = "jdbc:mysql://localhost:32771/prueba?useSSL=false";
+    String usuario = "admin";
+    String pasword = "12345678";
+    
+    public conector (){
+        
+       try {
+            conexion = (Connection) DriverManager.getConnection(url,usuario,pasword);
+            if(conexion != null){
+                System.out.println("Se conecto");
+            }else{
+                System.out.println("No se conecto");
+            }
+            
+        } catch (SQLException ex) {
+            System.err.println("Error de conexion");
+            ex.printStackTrace();
+        }
+
+    }
+    
+    public Connection getConxion(){
+        return conexion;
+    }
+    
+    public void Desconectars (){
+        conexion=null;
+    }
+    
+    
+    
+}
---
+++ conexionBDD/src/conexionbdd/ConexionBDD.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionbdd;
+
+import conexion.conector;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author crist
+ */
+public class ConexionBDD {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PreparedStatement ps = null;
+        conector stringConexion = new conector();
+        stringConexion.getConxion();
+        
+        // Insert
+        
+        String insert =
+         "insert into "
+         + "articulos(nombre,descripcion,precio) "
+         + "values(?,?,?) ";
+        
+        try {
+            ps = stringConexion
+                    .getConxion()
+                    .prepareStatement(insert);
+            ps.setString(1, "X box");
+            ps.setString(2, "Consola juegos");
+            ps.setInt(3, 500);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
