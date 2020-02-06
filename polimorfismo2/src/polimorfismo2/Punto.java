7513c478896cc75dff97024ca258e1ac7d3a6953
cristian97406 <59977082+cristian97406@users.noreply.github.com>
2020-02-06 09:29:39 -0500

    dibujo

--- polimorfismo2/src/polimorfismo2/Punto.java
+++ polimorfismo2/src/polimorfismo2/Punto.java
@@ -65,7 +65,7 @@ public class Punto extends Figura {
 
     @Override
     public double area() {
-       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     
